d054e3c3814900aeb3d58044492d385797a5c0ad
rocassius <rocassius@berkeley.edu>
2020-06-30 10:01:02 -0700

    created main file

--- main.py
+++ main.py
@@ -1,22 +1,10 @@
 import os
 import sys
 import urllib.request
-from data_util import config 
-# from summarizer import *
-
-from google.cloud import storage
-# sys.path.append(os.path.abspath('data'))
 
 # Fetch Model
 model_url = 'https://storage.cloud.google.com/extraction_bucket_1/saved_models/0000360.tar'
 model_file, headers = urllib.request.urlretrieve(model_url, filename='/tmp/model.tar')
-
-# Create Summarizer
-# summarizer  = Summarizer(
-#   vocab_path = '/data/vocab/vocab.txt'),
-#   model_path = model_file,
-#   model = TaskModel
-# )
 
 def summarize(request):
   request_json = request.get_json(silent=True)
